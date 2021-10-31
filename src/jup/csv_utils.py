b4496304ff406df5108b6f948e62d4dbf21eb36e
Marina Beshai <mbeshai@princeton.edu>
2021-10-31 07:50:47 -0400

    JUPPP

---
+++ src/jup/csv_utils.py
@@ -0,0 +1,62 @@
+import os
+from utils import make_dir, isvalid, get_filename
+import csv
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# @TODO
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def make_csv_breakdown(path_csv, filename, d,  key_header):
+    wd = get_filename(path_csv, filename)
+
+    with open(wd, 'w') as csvfile:
+
+        filewriter = csv.writer(csvfile)
+
+        values = []
+        for d2 in d.values():
+            for v in d2:
+                #  gets rid of nan.
+                if isvalid(v) and v not in values:
+                    values.append(v)
+
+        values.sort()
+        values.insert(0, key_header)
+        # values.insert(1, -1)
+        filewriter.writerow(values)
+        values.remove(key_header)
+
+        for k, d2 in zip(d.keys(), d.values()):
+            row = [""]*(len(values)+1)
+
+            # If there is a dictionary, we begin by adding the congressperson's name to the row.
+            if d2:
+                row.insert(0, k)
+
+            # Then for each date, we
+            for y in d2:
+                if isvalid(y):
+                    row[values.index(y) + 1] = d2[y]
+                # else:
+                #     row[values.index(-1) + 1] = d2[y]
+
+            filewriter.writerow(row)
+    return wd 
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# @TODO
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def make_csv(path_csv, filename, d, key_header, value_header):
+
+    wd = get_filename(path_csv, filename)
+    
+    with open(wd, 'w') as csvfile:
+        filewriter = csv.writer(csvfile)
+
+        filewriter.writerow([key_header, value_header])
+
+        for k, v in zip(d.keys(), d.values()):
+            filewriter.writerow([k, v])
+            
+    return wd
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- src/py/check.py
+++
@@ -1,7 +0,0 @@
-from parse.dates import format_transaction_date_search
-
-
-if __name__ == '__main__':
-    
-    print(format_transaction_date_search('01/07/2011'))
-    print(format_transaction_date_search('1/7/2011'))
