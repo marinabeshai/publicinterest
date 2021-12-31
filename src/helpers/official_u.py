6d2776d46b7f28bcf3a4ca59a3922bcbebeacc7c
Marina Beshai <mbeshai@princeton.edu>
2021-12-31 15:37:39 -0500

    Random work.

--- src/helpers/official_u.py
+++ src/helpers/official_u.py
@@ -6,119 +6,68 @@ import re
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Assert that value is not NULL. 
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def not_null(val):
-    assert val is not None 
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Class for Official (either senator or representative).
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Official:
-    def __init__(self, paperwork_name,  jr, state, term_start, term_end, birth_date, birth_place, party, alma_mater, education):
-        self.paperwork_name = paperwork_name
-        self.jr = jr
+    def __init__(self, name, state, birth_date, birth_place, party, education, senate, house):
+        self.name = name
         self.state = state
-        self.term_start = term_start
-        self.term_end = term_end
         self.birth_date = birth_date
         self.birth_place = birth_place
         self.party = party
-        
-        # EITHER SENATOR OR REPRESENTATIVE [COULD HAVE BEEN BOTH :()]
-        # self.type =  ?????
-        
-        if alma_mater:
-            self.education = alma_mater
-        else:
-            self.education = education
+        self.education = education
+        self.senate = senate
+        self.house = house
         
     def check(self):
-        return {"self.get_paperworkname()" : self.get_paperworkname(),
+        return {"self.get_paperworkname()" : self.get_name(),
                 "self.get_congress()" : self.get_congress(), 
                 "self.get_birthdate()" : self.get_birthdate(), 
-                "self.get_jr()" : self.get_jr(), 
                 "self.get_state()" : self.get_state(),
-                "self.get_term_start()" : self.get_term_start(),
                 "self.get_birth_place()" : self.get_birth_place(), 
                 "self.get_party()" : self.get_party(), 
                 "self.get_education()" : self.get_education(), 
-                "self.get_num_of_years()" : self.get_num_of_years(),
+                "self.get_num_of_years()" : self.get_seniority(),
                 "self.get_num_of_degrees()": self.get_num_of_degrees()}
     
     def get_gender(self):
         d = gender.Detector()
-        x =  d.get_gender(self.paperwork_name.split(" ")[0])
+        x =  d.get_gender(self.name.split(" ")[0])
         if "mostly" in x:
             return x.split("_")[1]
         return x 
         
-    def get_paperworkname(self):
-        # not_null(self.paperwork_name)
-        return self.paperwork_name
-        
-    def get_congress(self):
-        # not_null(self.term_start)
-        if not self.term_start:
-            return None
-
-        try:
-            begin_congress = 93 + ((datetime.strptime(self.term_start, '%B %d, %Y').date().year - 1973) // 2)
-            if self.term_end:
-                end_congress = 93 + ((datetime.strptime(self.term_end, '%B %d, %Y').date().year - 1973) // 2)
-            else:
-                end_congress = 93 + ((date.today().year - 1973) // 2)
-
-            return list(range(begin_congress, end_congress+1)) 
-        except Exception:
-            return None
-
-        # 3000 - term_start
-    def get_birthdate(self):
-        if not self.birth_date:
-                return None
-
-        # not_null(self.birth_date)
-        return datetime.strptime(self.birth_date, '%Y/%m/%d').strftime(DATE_FORMAT)
-
-
-    def get_jr(self):
-        # not_null(self.jr)
-        return self.jr
-
+    def get_name(self):
+        assert self.name
+        return self.name
+   
     def get_state(self):
-        # not_null(self.state)
+        assert self.state
         return self.state
 
-    def get_term_start(self):
-        # not_null(self.term_start)
-        if not self.term_start:
-                return None
+    def get_birthdate(self):
+        assert self.birth_date
+        return datetime.strptime(self.birth_date, '%Y/%m/%d').strftime(DATE_FORMAT)
 
-        try:
-            return datetime.strptime(self.term_start, '%B %d, %Y').strftime(DATE_FORMAT)
-        except Exception:
-            return None
-        
     def get_birth_place(self):
-        # not_null(self.birth_place)
-        return self.birth_place
-
+        assert self.birth_place
+        return self.birth_place.split("|")[0].split("<")[0]
+    
     def get_party(self):
-        # not_null(self.party)
+        assert self.party 
         return self.party
 
     # Gives you a rough list of degrees per official. 
     def get_education(self):
+        # assert self.education
+
+        nl = []
         if not self.education:
-            return None
-        # not_null(self.education)
+            return nl 
 
         l = self.education.replace("\n", " ").replace("|", " ").replace("<br>", " ").replace("<br/>", " ").replace("<br />", " ").split(" ")
         
         s = ""
-        nl = []
         for word in l:
             s += word     
             s += " "
@@ -127,42 +76,81 @@ class Official:
                 nl.append(s.strip())
                 s = ""
 
-        # assert nl != []
         return nl 
+
+
+    def get_congress(self):
+            assert self.senate or self.house
+
+            l = [self.senate, self.house]
+            ranges = []
+            res = []
+            
+            for phrase in l:
+                if not phrase: 
+                    continue
+                if "," in phrase:
+                    lprime = phrase.split(",")
+                    for i in lprime: ranges.append(i) 
+                else:
+                    ranges.append(phrase) 
+                    
+            for r in ranges: 
+                l = r.split("-")
+                x = l[0]
+                y = l[1]
+                
+                if y == 'Present': y = date.today().year
+
+                begin_congress = 93 + ((int(x) - 1973) // 2)
+                end_congress = 93 + ((int(y) - 1973) // 2)
+                res.append(list(range(begin_congress, end_congress+1)))
+
+
+            return list(set(item for sublist in res for item in sublist))
         
     def get_num_of_degrees(self):
-        # not_null(self.education)
-        if not self.education:
-                return None
         try:
-            ed = str(self.get_education())
-            key = ["BS", "BA", "JD", "AB", "MAR", "MD", "MS", "OD", "PhD", "AAS", "MSS", "BCL", "MPP", "BBA", "LLM", "MBA", "MA", "LLB","MEd"]
+            ed = str(self.get_education()).replace("'","").replace("[", "").replace("]", "").split(" ")
+            key = ["AA", "J.D.", "B.A.", "BS", "BA", "JD", "AB", "MAR", "MD", "MS", "OD", "PhD", "AAS", "MSS", "BCL", "MPP", "BBA", "LLM", "MBA", "MA", "LLB","MEd", "AS", "EdD", "MPA", "MSc", "MPH", "SM", "MPhil", "MAcc", "MIA", "ALB", "BDiv", "DVM", "BEng"]
             count = 0
             
-            for k in key:
-                where_to_start = 0
-                while ed.find(k, where_to_start) > 0:
-                    x = ed.find(k, where_to_start)
-                    if ed[ x - 1: x+len(k)] not in key and ed[x: x+len(k)] == k and ed[ x: x+len(k)+1] not in key:
-                        count += 1 
-                    where_to_start = x + 1 
-                    
+            for word in ed:
+                if word[len(word) - 1] == ")" or word[len(word) - 1] == ",":
+                    word = word[ : len(word) - 1]
+                    ed.append(word)
+                    continue
+                if len(word) > 5: 
+                    continue
+                if word in key:
+                    count += 1 
                     
             return count 
         except:
             return None
 
-    def get_num_of_years(self):
-        if not self.term_start:
-                return None
+    def get_seniority(self):
+        assert self.senate or self.house
+        
+        l = [self.senate, self.house]
+        years = 0 
+        
+        for range in l:
+            if not range: 
+                continue
+            if "," in range:
+                lprime = range.split(",")
+                for i in lprime: l.append(i) 
+                
+                continue
+            x, y = range.split("-")
+            if y == "Present":
+                y = date.today().year
+                
+            years += int(y) - int(x)    
+            
+        return years if years > 0 else 1 
 
-        try:
-            today = date.today()
-            term_start = datetime.strptime(self.term_start, '%B %d, %Y').date()
-            diff = today.year - term_start.year
-            return diff if diff != 0 else 1
-        except:
-            return None 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -191,33 +179,29 @@ def is_roman_number(num):
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_canonical_name(name):
     try:
-        if name == 'Ron L Wyden' or name == 'Ron Wyden':
-            name = 'Ronald L Wyden'
-        if name == 'Angus S King':
-            name = 'Angus S King, Jr.'
-        if name == 'Roy Blunt':
-            name = 'Roy D Blunt'
-        if name == 'Hon. Charles J. "Chuck" Fleischmann':
-            name = 'Hon. Charles J. Fleischmann'
-        if name == 'Hon. Neal Patrick Dunn MD, FACS' or name == 'Hon. Neal Patrick MD, Facs Dunn' or name == 'Hon. Neal Patrick MD, FACS Dunn':
-            name =  'Hon. Charles J. Fleischmann'
-        if name == 'Hon. James E Hon Banks':
-            name = 'Hon. James E. Banks'    
-        if name == 'Maria Cantwell':
-            name = 'Maria E Cantwell'
-        if name == 'Hon. Wm. Lacy Clay':
-            name = 'Hon. William Lacy Clay Jr.'
-        if name == 'Tammy Duckworth':
-            name = 'Ladda Tammy Duckworth'
-        if name == 'Angus S King':
-            name = 'Angus S King, Jr.'
-        if name == 'Tom S Udall' or name == 'Thomas Udall':
-            name = 'Thomas S Udall'
-        if name == 'Hon. Greg Steube' or name == 'Hon. W. Greg Steube':
-            name = 'Hon. William Greg Steube' 
-        if name == 'Hon. Michael Garcia':
-            name = 'Hon. Mike Garcia'
-
+        problematic = {
+            'Ron L Wyden' :'Ronald L Wyden',
+            'Ron Wyden':'Ronald L Wyden',            
+            'Angus S King': 'Angus S King, Jr.',            
+            'Roy Blunt' : 'Roy D Blunt',            
+            'Hon. Charles J. "Chuck" Fleischmann' : 'Hon. Charles J. Fleischmann',
+            'Hon. Neal Patrick Dunn MD, FACS' :  'Hon. Charles J. Fleischmann' ,
+            'Hon. Neal Patrick MD, Facs Dunn' :  'Hon. Charles J. Fleischmann' , 
+            'Hon. Neal Patrick MD, FACS Dunn' : 'Hon. Charles J. Fleischmann',
+            'Hon. James E Hon Banks' : 'Hon. James E. Banks'    ,
+            'Maria Cantwell' : 'Maria E Cantwell',
+            'Hon. Wm. Lacy Clay' : 'Hon. William Lacy Clay Jr.',
+            'Tammy Duckworth' : 'Ladda Tammy Duckworth',
+            'Angus S King' : 'Angus S King, Jr.',
+            'Tom S Udall' : 'Thomas S Udall',
+            'Thomas Udall': 'Thomas S Udall',
+            'Hon. Greg Steube'  : 'Hon. William Greg Steube' ,
+            'Hon. W. Greg Steube'  : 'Hon. William Greg Steube' ,
+            'Hon. Michael Garcia' : 'Hon. Mike Garcia',           
+            'Hon. Cindy Axne' : 'Hon. Cynthia Axne'}
+            
+        if name in problematic:
+            name = problematic[name]
         name = name.replace('Hon. ', '').replace('Mr. ', '').replace('Honorable', '').replace('Mrs. ', '').replace('None ', '').replace('Hon ', '').replace('Dr ', '')
         
         nl = (name.replace(",", "").split(" "))
--- src/helpers/search_u.py
+++ src/helpers/search_u.py
@@ -17,19 +17,44 @@ def get_yfinance(ticker, industry=True):
     assert ticker != '--'
     
     try:
-        
+        if '.' in ticker: 
+            ticker = ticker[:ticker.find('.')]
         if industry:
             key = "Industry"
         else:
             key = "Sector(s)"
             
-        if ticker == "BTC":
+        if ticker == "BTC" or ticker == 'DOGE-USD':
             return "cryptocurrency"
         
+        if ticker == 'URGO' or ticker == 'WXA.F':
+            return None 
+        
+        if ticker == 'SPDR':
+            ticker = 'SPY'
+            
+        if ticker == 'BLFSD':
+            ticker = 'BLFS'
+            
+        if ticker == 'XER.BE':
+            ticker = 'XER2.BE'
+            
+        if ticker == 'BP PLC':
+            ticker = 'BP'
+            
+        if ticker == 'EBJ':
+            ticker = 'ERJ'
+        
         # Corner cases.
-        if ticker == "ETHE" or ticker == "GBTC" or ticker == "QQQ" or ticker == 'BSTZ':
+        if ticker == "ETHE" or ticker == "GBTC" or ticker == "QQQ" or ticker == 'BSTZ' or ticker == 'BMEZ':
             return "Trust"
         
+        if ticker == 'VMCXX' or ticker == 'FDRXX' or ticker == 'SNOXX':
+            return 'Fund'
+        
+        if ticker == 'RF$A':
+            ticker = 'RF'
+            
         if ticker == "RDSA":
             ticker = "RDS-A"
             
@@ -39,8 +64,17 @@ def get_yfinance(ticker, industry=True):
         # Copied Axon.
         if ticker == "FL4.SG":
             return "Aerospace & Defense" if industry else "Industrials"
-            
-            
+        if ticker == "SYY.SG":
+            return "Food Distribution" if industry else "Consumer Defensive"
+        if ticker == 'NTXFY':
+            return 'Financial Services' if industry else 'Banks - Regional'
+        if ticker == 'PRSP':
+            return 'IT Services' if industry else 'Information Technology'
+        if ticker == 'PS':
+            return 'Software' if industry else 'Information Technology'
+
+
+# quite surprising to see international markets 
         url = "https://finance.yahoo.com/quote/TICKER/profile?p=TICKER".replace("TICKER", str(ticker))
         agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15'
 
@@ -91,8 +125,9 @@ def get_yfinance(ticker, industry=True):
         return None 
     
     except Exception:
-        print(ticker)
-        raise Unknown
+        # print(ticker)
+        return None 
+        # raise Unknown
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -111,7 +146,8 @@ def get_industry(ticker):
             res = get_yfinance(ticker, industry=True)
             if res:
                 return res
-            raise Unknown
+            return None 
+            # raise Unknown
 
         if "429" in str(e):
             time.sleep(60)
@@ -133,7 +169,8 @@ def get_sector(ticker):
             res = get_yfinance(ticker, industry=False)
             if res:
                 return res
-            raise Unknown
+            # raise Unknown
+            return None 
 
         if "429" in str(e):
             time.sleep(60)
@@ -156,64 +193,48 @@ def clean_up_res(res):
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def parse(d, word, count, s, break_point):
     # Corner Case
+    # print(word)
+    
     if count == "":
         word = word + " "
     
     index = s.find(word+count)
+    # print(index)
     
     if index == -1:
         return d 
+    
     where_to_stop = len(word+count) + index
-
+    # print(where_to_stop)
+    
     while s[where_to_stop] != break_point:
         where_to_stop += 1
 
+    # print(where_to_stop)
+    
     res = s[index+len(word): where_to_stop]
-
+    # print(res) 
+    
     if not res and word != "term_start":
-        print(word)
-        print(s)
+        # print(word)
+        # print(s)
         raise Exception("Pattern mismatch.")
 
     else:
+        if 'Representatives' in word: 
+            word = 'state'
         d[word.strip()] = clean_up_res(res)
 
     return d
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def terms(s):
-    d = {}
-
-    print(s)
-    
-    jr = "jr/sr"
-    term_start = "term_start"
-    term_end = "term_end"
-    state = "state"
-    count = ""
-
-    if s[s.find(jr) + len(jr): s.find(jr) + len(jr)+1].isdigit():
-        count = s[s.find(jr) + len(jr): s.find(jr) + len(jr)+1]
-
-    d = parse(d, jr, count, s, "|")
-    d = parse(d, term_start, count, s, "|")
-    d = parse(d, term_end, count, s, "|")
-    d = parse(d, state, count, s, "|")
-
-    return d
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def go_shopping(name, l, s, d):
+def go_shopping(name, l, s):
     try:
-
+        d = {}
+        
         while l:
             w = l.pop()                
             i = s.find(w)
@@ -319,37 +340,120 @@ def wiki_search(name):
             name = 'Taylor, Nicholas Van'
         if name == 'Jacobs, Christopher L.':
             name = 'Chris Jacobs'
+        if name == 'Pfluger, August L.':
+            name = 'Pfluger, August'
+        if name == 'Hill, James F.':
+            name = 'French Hill'
             
         results = wikipedia.search(name)
         at = 0
-        s = ""
+        
+        s = page(results[at]) 
+        if "may refer to" in s: 
+            at += 1
+            s = page(results[at])  
+
 
         while "United States Senator" not in s and "United States representative" not in s and "U.S. representative" not in s and  "United States senator" not in s and "U.S. Representative" not in s and "U.S. House of Representatives" not in s and "United States House of Representatives" not in s:
-            s = page(results[at])            
             at += 1
+            s = page(results[at])  
+        
+        d = go_shopping(name, ["birth_place ", "alma_mater ", "birth_date ", "education "], s)
+        d = congress_gov_get(name, d)
 
-            
-        d = terms(s)
-        d = go_shopping(name, ["birth_place ", "party ", "alma_mater ", "birth_date ", "education "], s, d)
-
-
-        jr = d.get("jr/sr", None)
         birth_place = d.get("birth_place", None)
-        party = d.get("party", None)
-        alma_mater = d.get("alma_mater", None)
-        education = d.get("education", None)
+        
+        education = d.get("alma_mater", None)
+        if not education:
+            education = d.get("education", None)
+
+
         birth_date = d.get("birth_date", None)
-        term_start = d.get("term_start", None)
-        term_end = d.get("term_end", None)
         state = d.get("state", None)
-
-        x = Official(name, jr, state, term_start, term_end,
-                    birth_date, birth_place, party, alma_mater, education)
-
-        return x
+        party = d.get("party", None)
+        senate = d.get("senate", None)
+        house = d.get("house", None)
+    
+        return Official(name, state, birth_date, birth_place, party, education, senate, house)
         
     except Exception as e:
         print(str(e))
         print(name)
-        print(s)
+        # print(s)
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+def congress_gov_get(name, d):
+    # Remove middle initial
+    if name[len(name)-1] == ".":
+        name = name[:len(name)-3].strip()
+
+                
+    problematic = {"Allen, Richard" : "Allen, Rick", 
+                "Krishnamoorthi, S." : "Krishnamoorthi, Raja",
+                "Mcconnell Jr., A." : "McConnell, Mitch",
+                "Taylor, Nicholas Van" : "Taylor, Van",
+                "Fallon, Patrick" : "Fallon, Pat",
+                "Gallagher, Michael" : "Gallagher, Mike",
+                "Sullivan, Daniel" : "Sullivan, Dan",
+                "Crapo, Michael" : "Crapo, Mike",
+                "Hagerty IV, William" : "Hagerty, Bill",
+                "Duckworth, Ladda" : "Duckworth, Tammy",
+                "Khanna, Rohit" : "Khanna, Ro",
+                "Cruz, Rafael" : " Cruz, Ted",
+                "Wyden, Ronald" : "Wyden, Ron",
+                "Moore, Felix" : "Moore, Barry",
+                "Banks, James" : "Banks, Jim",
+                "Udall, Thomas" : "Udall, Tom",
+                "Conaway, K." : "Conaway, K. Michael",
+                "Perdue Jr., David" : "Perdue, david ",
+                "Crenshaw, Daniel" : "Crenshaw, Dan",
+                "Arenholz, Ashley" : "Hinson, Ashley",
+                "Steube, William" : "Steube, W. Gregory",
+                "Cassidy, William" : "Cassidy, Bill",
+                "Clay Jr., William" : "Clay, Wm. Lacy",
+                "Tuberville, Thomas" : "Tuberville, Tommy",
+                "Kaine, Timothy" : "Kaine, Tim",
+                "Hagedorn, James" : "Hagedorn, Jim",
+                "Portman, Robert" : "Portman, Rob" }
+    
+    if name in problematic: 
+        name = problematic[name]
+
+    htmled_name = name.replace(" ", "%20").replace(",", "%2C")
+    html = 'https://www.congress.gov/search?q=%7B"source"%3A"members"%2C"search"%3A"{name}"%7D'.format(name=htmled_name)
+    result = requests.get(html).text
+    
+    result = result[result.find('<div><span class="visualIndicator">MEMBER</span></div>') : result.find('<li class="compact" style="display:none">    1.')]
+    
+    state = result[result.find('<strong>State:</strong>') : ] 
+    state = state[ : state.find('</span>')]
+    state = state[state.find('<span>') : ].replace("<span>", "")
+
+    party = result[result.find('<strong>Party:</strong>') : ] 
+    party = party[ : party.find('</span>')]
+    party = party[party.find('<span>') : ].replace("<span>", "")
+
+
+    served = result[result.find('<strong >Served:</strong>') : ] 
+    served = served[ : served.find('</span>')]
+    served = served[served.find('<span>') : ].replace("<span>", "").replace('<ul class="member-served">\n', "").replace("<li>", "")
+    
+    senate = house = None 
+    if served.find("Senate: ")  > 0: 
+        served_temp = served[served.find("Senate: ") + 8 : ] 
+        senate = served_temp [ : served_temp.find("</li>")]
+        
+    if served.find("House: ")  > 0: 
+        served_temp = served[served.find("House: ") + 7 : ] 
+        house = served_temp [ : served_temp.find("</li>")]
+
+    d['state'] = state
+    d['party'] = party
+    d['senate'] = senate
+    d['house'] = house
+
+    return d 
+        
